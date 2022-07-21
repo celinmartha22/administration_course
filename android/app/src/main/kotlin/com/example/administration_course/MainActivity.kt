4e3ed99ffa7c933dba1183e9c61d7c05cf286330
DESKTOP-ASL0NUJ\Celine Cantik <celinmartha22@gmail.com>
2022-07-21 16:24:14 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/administration_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.administration_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class SiteLayout extends StatelessWidget {
+  const SiteLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
